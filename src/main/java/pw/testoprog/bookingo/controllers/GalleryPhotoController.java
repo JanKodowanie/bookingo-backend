36e0d0e3ff6b4c2fc48ae1dfba332fb1e8ab53ab
jan-polaczek <jan.polaczek@interia.pl>
2020-11-15 17:09:23 +0100

    added empty model and controller

---
+++ src/main/java/pw/testoprog/bookingo/controllers/GalleryPhotoController.java
@@ -0,0 +1,7 @@
+package pw.testoprog.bookingo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GalleryPhotoController {
+}
---
+++ src/main/java/pw/testoprog/bookingo/models/GalleryPhoto.java
@@ -0,0 +1,7 @@
+package pw.testoprog.bookingo.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class GalleryPhoto {
+}
